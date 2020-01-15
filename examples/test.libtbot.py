9499773ec6beb624dd9e3d0985f1f84fca2f97c4
Chen Li <chencassc@gmail.com>
2020-01-15 13:08:38 +0800

    add some apis

--- examples/test.libtbot.py
+++ examples/test.libtbot.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 import logging
 import websocket
 import traceback
@@ -24,6 +26,50 @@ except ImportError:
  MOTOR_CMD_REPORT_STOP_RUN,
  *_)  = range(100)
 
+
+(GPIO_CMD_INVALID,
+ GPIO_CMD_OUT_HIGH,
+ GPIO_CMD_OUT_LOW,
+ GPIO_CMD_IN_PULLUP,
+ GPIO_CMD_IN_PULLDOWN,
+ GPIO_CMD_IN_NOPULL,*_) = range(100)
+
+(ADC_CMD_INVALID,
+ ADC_CMD_GET_ADC,
+ ADC_CMD_GPIO_OUT_HIGH,
+ ADC_CMD_GPIO_OUT_LOW,
+ ADC_CMD_GPIO_IN_PULLUP,
+ ADC_CMD_GPIO_IN_PULLDOWN,
+ ADC_CMD_GPIO_IN_NOPULL,*_) = range(100)
+
+# 外设接口
+(TBOT_PORT_INVALID,
+ TBOT_PORT_I2C_UART_1,
+ TBOT_PORT_I2C_UART_2,
+ TBOT_PORT_I2C_UART_3,
+ TBOT_PORT_I2C_UART_4,
+ TBOT_PORT_I2C_UART_5,
+ TBOT_PORT_I2C_UART_6,
+ TBOT_PORT_MOTOR_1,
+ TBOT_PORT_MOTOR_2,
+ TBOT_PORT_MOTOR_3,
+ TBOT_PORT_MOTOR_4,
+ TBOT_PORT_MOTOR_5,
+ TBOT_PORT_MOTOR_6,
+ TBOT_PORT_PWM_1,
+ TBOT_PORT_PWM_2,
+ TBOT_PORT_PWM_3,
+ TBOT_PORT_PWM_4,
+ TBOT_PORT_ADC_1,
+ TBOT_PORT_ADC_2,
+ TBOT_PORT_ADC_3,
+ TBOT_PORT_ADC_4,
+ TBOT_PORT_GPIO_1,
+ TBOT_PORT_GPIO_2,
+ TBOT_PORT_GPIO_3,
+ TBOT_PORT_GPIO_4,
+ TBOT_PORT_MAX,
+ *_) = range(100)
     
 class XferHdrT(Structure):
     _fields_ = [('magic', c_uint8),
@@ -41,11 +87,10 @@ class XferHdrT(Structure):
 
 def hal_handler(data, length):
     # Pointer instances have a contents attribute which returns the object to which the pointer points,
-    print('recv hal data {}'.format(data))
-    print('recv hal len {}'.format(length))
-    print('recv hal data.contents {}'.format(data.contents))
-    print('recv hal data.contents.cmd {}'.format(data.contents.cmd))
-    print('recv hal data.contents.data.len {}'.format(data.contents.len))
+    data = data[0:length]
+    edata = json.loads(data)
+    print('get data: {}'.format(edata))
+    
     
 lib_path = '/lib/arm-linux-gnueabihf/libtbot.so'
 lib = cdll.LoadLibrary(lib_path)
@@ -53,4 +98,19 @@ REGISTER_RECVCB = CFUNCTYPE(c_void_p, c_char_p, c_uint32)
 cb = REGISTER_RECVCB(hal_handler)
 lib.register_recvcb(cb)
 lib.tbot_lib_init()
+
+time.sleep(2)
+# int tbot_adc_control(tbot_port_t port,  enum adc_cmd cmd, uint32_t *adc_mv, uint32_t timeout_ms);
+val = c_uint32()
+lib.tbot_adc_control(TBOT_PORT_ADC_1, ADC_CMD_GET_ADC, byref(val), c_uint32(3000))
+print('adc val {}'.format(val.value))
+
+pin_val = c_uint8()
+# int tbot_gpio_control(tbot_port_t port,  enum gpio_cmd cmd, uint8_t *gpio_in_val, uint32_t timeout_ms);
+lib.tbot_gpio_control(TBOT_PORT_GPIO_1, GPIO_CMD_OUT_LOW, byref(pin_val), c_uint32(3000));
+print('pin val {}'.format(pin_val.value))
+time.sleep(2)
+lib.tbot_gpio_control(TBOT_PORT_GPIO_1, GPIO_CMD_OUT_HIGH, byref(pin_val), c_uint32(3000));
+print('pin val {}'.format(pin_val.value))
+
 time.sleep(15)
